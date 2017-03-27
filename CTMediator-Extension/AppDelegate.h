1e27f84d7044335485d0ef69d18b5252283a22d6
gzyCat <841905781@qq.com>
2017-03-27 11:19:30 +0800

    Initial Commit

---
+++ CTMediator-Extension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTMediator-Extension
+//
+//  Created by yiyizuche on 2017/3/27.
+//  Copyright © 2017年 yyzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
